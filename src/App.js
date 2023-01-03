0dc08b0eb68930d5fdb89878a23e52ae7d4c5856
athulpm <97athulpm@gmail.com>
2023-01-03 14:16:59 +0530

    Added and styled meals summary component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./Components/Layout/Header";
-
+import MealsSummary from "./Components/Layout/MealsSummary";
 
 function App() {
   return (
     <div>
       <Header/>
+      <MealsSummary/>
     </div>
   );
 }
